978ff901427bf4b838946ec2ed592190d92518bc
S-Lombardi <silo.lombardi@gmail.com>
2023-07-27 09:30:34 +0200

    Correzione nome colonna

--- database/migrations/2023_07_26_165057_add_data_to_trains_table.php
+++ database/migrations/2023_07_26_165057_add_data_to_trains_table.php
@@ -31,7 +31,7 @@ return new class extends Migration
     {
         Schema::table('trains', function (Blueprint $table) {
             $table->dropColumn('data_partenza');
-            $table->dropColumn('data_partenza');
+            $table->dropColumn('data_odierna');
         });
     }
 };
