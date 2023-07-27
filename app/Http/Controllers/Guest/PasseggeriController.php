9ee60543d144453d97abd4cd7b8d8ef602287da4
S-Lombardi <silo.lombardi@gmail.com>
2023-07-27 18:35:29 +0200

    creato Controller per tabella PAsseggeri

---
+++ app/Http/Controllers/Guest/PasseggeriController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Passeggeri;
+
+class PasseggeriController extends Controller
+{   
+    //Con funzione all() vado a prendere tutti i dati dal db    
+    public function index(){
+        $passeggeri = Passeggeri::all();
+        //dd($trains);
+        return view('home', compact ('passeggeri'));
+    }  
+}
